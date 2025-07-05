c39729d0a329bc84072f78665ce287008b9f78d6
Kitsu <kitsudevacc@gmail.com>
2025-07-05 16:01:18 +0200

    Added main for extra testing, last stable before branch

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int flame1 = 1;
+        int flame2 = 6;
+        int flame3 = 24;
+
+        System.out.println();
+
+        /// Prints out the output of an on-going small flame (flame with short timer)
+        System.out.println("---=====Example 1: On-going Small Fire=====---");
+        Campfire smallFire1 = new Campfire(List.of(new Fuel(flame1), new Flame("12:00", flame1)));
+        smallFire1.display("12:30");
+
+        System.out.println();
+
+        /// Prints out not-yet-started fire that lasts 6 hours (multiple twigs)
+        System.out.println("---=====Example 2: Future Medium Fire=====---");
+        Campfire mediumFire1 = new Campfire(List.of(new Fuel(flame2), new Flame("12:00", flame2)));
+        mediumFire1.display("08:00");
+
+        System.out.println();
+
+        /// Prints out already finished large fire (log)
+        System.out.println("---=====Example 3: Past Large Fire=====---");
+        Campfire largeFire1 = new Campfire(List.of(new Fuel(flame3), new Flame("12:00", flame3)));
+        largeFire1.display("13:00");
+    }
+}
